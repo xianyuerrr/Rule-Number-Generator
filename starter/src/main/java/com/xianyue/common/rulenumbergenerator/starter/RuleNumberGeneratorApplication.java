79e3fe4be5778a1876902c82738e6134a2526d61
xianyuerrr <xianyuerrr@gmail.com>
2023-06-11 20:43:16 +0800

    日志配置-logback

--- starter/src/main/java/com/xianyue/common/rulenumbergenerator/starter/RuleNumberGeneratorApplication.java
+++ starter/src/main/java/com/xianyue/common/rulenumbergenerator/starter/RuleNumberGeneratorApplication.java
@@ -1,14 +1,15 @@
 package com.xianyue.common.rulenumbergenerator.starter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = "com.xianyue")
+@Slf4j
 public class RuleNumberGeneratorApplication {
-     public static ApplicationContext applicationContext;
 	public static void main(String[] args) {
-		applicationContext = SpringApplication.run(RuleNumberGeneratorApplication.class, args);
-
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(RuleNumberGeneratorApplication.class, args);
+		log.info("Spring boot启动初始化了 {} 个 Bean", applicationContext.getBeanDefinitionNames().length);
 	}
 }
