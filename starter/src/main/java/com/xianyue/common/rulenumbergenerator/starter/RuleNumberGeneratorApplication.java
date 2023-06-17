06beb534f86d6c17d86283e6902b283369f4d7a4
xianyuerrr <xianyuerrr@gmail.com>
2023-06-17 22:02:37 +0800

    数据库层测试

--- starter/src/main/java/com/xianyue/common/rulenumbergenerator/starter/RuleNumberGeneratorApplication.java
+++ starter/src/main/java/com/xianyue/common/rulenumbergenerator/starter/RuleNumberGeneratorApplication.java
@@ -1,19 +1,62 @@
 package com.xianyue.common.rulenumbergenerator.starter;
 
+import jakarta.persistence.EntityManagerFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+
+/**
+ * JPAConfig写在com.xianyue.common.rulenumbergenerator.infrastructure.config是无法正常工作的 FIXME
+ *
+ */
 @Slf4j
-@EntityScan(basePackages = {"com.xianyue"})
-@EnableJpaRepositories(basePackages = "com.xianyue")
+@EnableTransactionManagement
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.xianyue", transactionManagerRef = "jpaTransactionManager")
 @SpringBootApplication(scanBasePackages = "com.xianyue")
 public class RuleNumberGeneratorApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(RuleNumberGeneratorApplication.class, args);
 		log.info("Spring boot启动初始化了 {} 个 Bean", applicationContext.getBeanDefinitionNames().length);
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		HibernateJpaVendorAdapter japVendor = new HibernateJpaVendorAdapter();
+		japVendor.setGenerateDdl(false);
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource());
+		entityManagerFactory.setJpaVendorAdapter(japVendor);
+		// 与 @EntityScan(basePackages = {"com.xianyue.common.rulenumbergenerator.domain.*.entity"}) 都是用来扫描实体的
+		entityManagerFactory.setPackagesToScan("com.xianyue.common.rulenumbergenerator.domain.**.entity");
+		return entityManagerFactory;
+	}
+
+	@Bean
+	public PlatformTransactionManager jpaTransactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
 	}
 }
--- starter/src/main/resources/db/schema.sql
+++ starter/src/main/resources/db/schema.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS `orm_user_dept`;
+CREATE TABLE `orm_user_dept` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
+ `user_id` INT(11) NOT NULL COMMENT '用户id',
+ `dept_id` INT(11) NOT NULL COMMENT '部门id',
+ `create_time` DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+ `last_update_time` DATETIME NOT NULL DEFAULT NOW() COMMENT '上次更新时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Spring Boot Demo Orm 系列示例表';
+
 DROP TABLE IF EXISTS `orm_user`;
 CREATE TABLE `orm_user` (
   `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
@@ -20,15 +29,6 @@ CREATE TABLE `orm_department` (
   `superior` INT(11)  COMMENT '上级id',
   `levels` INT(11) NOT NULL COMMENT '层级',
   `order_no` INT(11) NOT NULL DEFAULT 0 COMMENT '排序',
-  `create_time` DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
-  `last_update_time` DATETIME NOT NULL DEFAULT NOW() COMMENT '上次更新时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Spring Boot Demo Orm 系列示例表';
-
-DROP TABLE IF EXISTS `orm_user_dept`;
-CREATE TABLE `orm_user_dept` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
-  `user_id` INT(11) NOT NULL COMMENT '用户id',
-  `dept_id` INT(11) NOT NULL COMMENT '部门id',
   `create_time` DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
   `last_update_time` DATETIME NOT NULL DEFAULT NOW() COMMENT '上次更新时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Spring Boot Demo Orm 系列示例表';
