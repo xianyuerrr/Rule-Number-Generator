592c1140c249e06ea65432daaa92ce046a956be1
xianyuerrr <xianyuerrr@gmail.com>
2023-06-17 12:19:14 +0800

    通用返回优化

--- common/src/main/java/com/xianyue/common/core/response/ApiResponse.java
+++ common/src/main/java/com/xianyue/common/core/response/ApiResponse.java
@@ -25,15 +25,37 @@ public class ApiResponse<T> {
         this(status, data, null);
     }
 
+    /**
+     * 供业务代码使用
+     *
+     * @param data 需要返回的数据
+     * @param <T> 需要返回的数据的类型
+     * @return Api通用返回
+     */
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse(ReturnStatus.SUCCESS, data);
     }
 
+    /**
+     * 供异常处理类使用
+     *
+     * @param errors 需要返回的错误信息
+     * @param <T> 需要返回的数据的类型
+     * @return Api通用返回
+     */
     public static <T> ApiResponse<T> fail(List<Error> errors) {
         Objects.requireNonNull(errors);
         return new ApiResponse<>(ReturnStatus.ERROR, null, errors);
     }
 
+    /**
+     * 供异常处理类使用
+     *
+     * @param data 需要返回的数据
+     * @param errors 需要返回的错误信息
+     * @param <T> 需要返回的数据的类型
+     * @return Api通用返回
+     */
     public static <T> ApiResponse<T> partialSuccess(T data, List<Error> errors) {
         Objects.requireNonNull(errors);
         return new ApiResponse<>(ReturnStatus.PARTIAL_SUCCESS, data, errors);
